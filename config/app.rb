a3ff5af8b87991e031b93558ae048923ab15e172
Luca G. Soave <luca.soave@gmail.com>
2012-08-10 21:02:01 +0200

    add MONGOLAB_URI config to config/app.rb and config/mongoid.yml

--- config/app.rb
+++ config/app.rb
@@ -1,6 +1,15 @@
 #this should live in a file called app.rb inside a directory called 'config'
 
+#config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
+#  conn = EM::Mongo::Connection.new('localhost', 27017, 1, {:reconnect_in => 1})
+# conn.db('db')
+#nd
+
 config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
-  conn = EM::Mongo::Connection.new('localhost', 27017, 1, {:reconnect_in => 1})
-  conn.db('db')
+  mongolab = URI.parse(ENV['MONGOLAB_URI'])
+  conn = EM::Mongo::Connection.new mongolab.host, mongolab.port, 1, {:reconnect_in => 1}
+
+  db = conn.db mongolab.path.gsub(/^\//, '')
+  db.authenticate mongolab.user, mongolab.password
+  db
 end
